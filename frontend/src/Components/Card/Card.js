689c6144c0396728d5916ebc92206ded84cef5eb
SatvikSingh <64217296+SatvikSingh@users.noreply.github.com>
2022-07-30 13:20:27 +0530

    added comments + final touches

--- frontend/src/Components/Card/Card.js
+++ frontend/src/Components/Card/Card.js
@@ -1,7 +1,9 @@
+// importing required libraries and packages
 import React from "react";
 import "./Card.css";
 import html2canvas from "html2canvas";
 
+// dummy data for the card
 const data = {
   name: "Satvik Singh",
   profession: "Software Developer",
@@ -12,17 +14,26 @@ const data = {
 };
 
 const Card = () => {
+
+  // function to export the card as a png image
   const share = async () => {
+    // if there is no share function in navigator, then return
     if (!("share" in navigator)) {
       return;
     }
+
+    // create a canvas element and render the card to it based on class provided
     const canvas = await html2canvas(document.querySelector(".share-content"));
+    // create a blob from the canvas
     canvas.toBlob(async (blob) => {
+      // create a files array containing the blob
       const files = [new File([blob], "image.png", { type: blob.type })];
+      // share the files with a text message
       const shareData = {
         text: "Checkout my business card!",
         files,
       };
+      // if the share function is available, then share the files
       if (navigator.canShare(shareData)) {
         try {
           await navigator.share(shareData);
@@ -31,7 +42,9 @@ const Card = () => {
             console.error(err.name, err.message);
           }
         }
-      } else {
+      } 
+      // if the share function is not available, then alert user that sharing is not available
+      else {
         console.warn("Sharing not supported", shareData);
       }
     });
@@ -39,7 +52,10 @@ const Card = () => {
 
   return (
     <div>
+      {/* Share Content */}
       <div className="share-content">
+  
+        {/* Business Card Front */}
         <div className="business-card">
           <div className="content">
             <div className="top">
@@ -61,6 +77,7 @@ const Card = () => {
           </div>
         </div>
 
+        {/* Business Card Back */}
         <div className="business-card">
           <div className="content">
             <div className="top">
@@ -83,6 +100,7 @@ const Card = () => {
         </div>
       </div>
 
+      {/* Share Button */}
       <div className="btn-wrap">
         <button className="button" onClick={share}>
           Share Image
